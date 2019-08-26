0071826260d817a7372a9df2095b898a8922900f
Nate V <nate.tripblazr@gmail.com>
2019-08-26 15:42:21 -0500

    added js

---
+++ src/scripts/main.js
@@ -0,0 +1,11 @@
+let journiArray = [];
+
+let journalEntry = {
+	date: undefined,
+	mood: undefined,
+	concepts: undefined,
+	text: undefined
+};
+
+journiArray.push(journalEntry);
+console.log(journiArray);
